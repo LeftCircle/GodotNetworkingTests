35ee63b75268ce01da13e57e9eac4f36e1bae989
LeftCircle <rcato60@gmail.com>
2025-05-02 11:46:22 -0400

    Does it work?

---
+++ scenes/ui/login_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+class_name LoginScreen
+
+
+func _on_host_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_client_pressed() -> void:
+	pass # Replace with function body.
